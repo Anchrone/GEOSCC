105a63effa91ec57ddf3330ca5a430069cedd14f
Anchrone <anchrone@gmail.com>
2018-04-21 21:30:13 +0400

    Add files via upload

---
+++ FTM.py
@@ -0,0 +1,8 @@
+
+print ("FTM v0.1. Works on Python 2. Optimization for Python 3 soon.")
+print ("This program is intended to convert feet to meters.")
+foot = float(input("Please input feet: "))
+meters = float(foot * .3048)
+
+print ("It is %f meters" %meters) 
+
