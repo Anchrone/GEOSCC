5df2b107666f4b7c7712ca5c2de4f3f2e6144896
Anchrone <anchrone@gmail.com>
2018-04-22 12:47:35 +0400

    Update mtf.py

--- mtf.py
+++ mtf.py
@@ -1,6 +1,5 @@
 
-print ("MTF v0.1. ")
-print ("This program is intended to convert meters to feet.")
+print ("This is intended to convert meters to feet.")
 meters = float(input("Please input meters: "))
 foot = float(meters * 3.28)
 
