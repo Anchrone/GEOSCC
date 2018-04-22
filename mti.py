9e26df5ae84a2fa999a32d898592ec78c868c541
Anchrone <anchrone@gmail.com>
2018-04-22 12:43:10 +0400

    Add files via upload

---
+++ mti.py
@@ -0,0 +1,5 @@
+print ("MTi 0.1 | This program is intended for meter-inch conversion")
+meters = float(input("Please input meters: "))
+cms = float(meters * 100)
+inches = float(cms * .3937)
+print ("It is %f" %inches ,"inches")
