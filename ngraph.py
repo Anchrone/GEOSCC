48bda2f5ec64290b1de99bb71a330231f8edf5fe
Anchrone <anchrone@gmail.com>
2018-11-18 01:36:41 +0400

    Update ngraph.py
    removed some clutter, introduced more intuitive input

--- ngraph.py
+++ ngraph.py
@@ -1,26 +1,22 @@
-#Ngraph v1.7
+#Ngraph v1.8
 #brought to you by yours truly @anchrone
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import interpolate
 
-#Start with years
-
 productionStartYear = int(input("Input production start year: "))
 productionEndYear = int(input("Input production end year: "))
 productionTimeDif = ((productionEndYear - productionStartYear) + 1 )
 
-years = []
 discharges = []
 
+years = list(range(productionStartYear, productionEndYear+1))
 
-for a in range(productionStartYear,productionEndYear+1):
-        year_of_Qn = int(input("What is year? "))
-        years.append(year_of_Qn)
-        xQn = (float(input("What is discharge? ")))
-        discharges.append(xQn)
-        
-        
+
+for currentYear in range(productionStartYear, productionEndYear+1):
+    xQn = float(input("What was Qoil in year %d? " % (currentYear)))
+    discharges.append(xQn)
+
 rQn = [abs(y-x) for x,y in zip(discharges, discharges[1:])]
 
 xnew = np.linspace(min(years), max(years), 100)
@@ -46,10 +42,5 @@ plt.xlabel("years")
 plt.ylabel("Q")
 plt.show()
 
-
-
-msgOne = input("Press ENTER to look at your values: ")
-
-print (totalRqN,xMedian,xUp,xDown)
 msgTwo = input("Press ENTER to continue: ")
 quit()
