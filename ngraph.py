855c7b49a27a79401d4b0b5bcc4898b4d8b49142
Anchrone <anchrone@gmail.com>
2018-11-17 23:31:09 +0400

    Update ngraph.py
    added interpolation, removed most of the junk

--- ngraph.py
+++ ngraph.py
@@ -1,7 +1,8 @@
-#Ngraph v.1.5
-
+#Ngraph v1.7
+#brought to you by yours truly @anchrone
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import interpolate
 
 #Start with years
 
@@ -11,19 +12,23 @@ productionTimeDif = ((productionEndYear - productionStartYear) + 1 )
 
 years = []
 discharges = []
-#Reach = []
-#reachMax = []
-dischargesC = []
-#reachSum = (sum(reachMax)/(productionTimeDif - 1))
+
 
 for a in range(productionStartYear,productionEndYear+1):
         year_of_Qn = int(input("What is year? "))
         years.append(year_of_Qn)
-        xQn = float(input("What is discharge? "))
+        xQn = (float(input("What is discharge? ")))
         discharges.append(xQn)
         
         
 rQn = [abs(y-x) for x,y in zip(discharges, discharges[1:])]
+
+xnew = np.linspace(min(years), max(years), 100)
+
+tck = interpolate.splrep(years, discharges, k =5)
+y_smooth = interpolate.splev(xnew, tck)
+
+plt.plot(xnew, y_smooth, label = "Q", color="blue")
 
 totalRqN = ((sum(rQn))/productionTimeDif)
 xMedian = ((sum(discharges))/productionTimeDif)
@@ -32,36 +37,19 @@ xDown = (abs(xMedian - ((3 * totalRqN)/1.128)))
 dischargeUpCoef = [xUp] * len(years)
 dischargeDownCoef = [xDown] * len(years)
 dischargeMedianCoef =  [xMedian] * len(years)
-plt.plot(years, discharges, label ="Q", color = "blue")
 plt.plot(years, dischargeUpCoef, label = "x UP", color = "green")
 plt.plot(years, dischargeDownCoef, label = "x DOWN", color = "red")
 plt.plot(years, dischargeMedianCoef, label = "x MEDIAN", color ="yellow")
 plt.title("Q of oil graph")
-plt.legend()
+plt.legend(loc = "best")
+plt.xlabel("years")
+plt.ylabel("Q")
 plt.show()
 
-#for x in range (years[1], productionTimeDif):
- #               dischargesC.append(xQn)
- #       
 
 
-
-##ReachB =  discharges[0:productionTimeDif]
-#reachMax = list(np.array(ReachG)-np.array(ReachB))
-
-#reachNew = zip(discharges[1:]-discharges)
-#dischargesC = (discharges)
-#del dischargesC[0]
-
-
-#need to find xmedian, R, xUp and xDown     
-#xmedian = (sum(discharges)/productionTimeDif)
-#reachSum = (sum(reachMax)/(productionTimeDif - 1))
-#print (xmedian)
 msgOne = input("Press ENTER to look at your values: ")
 
 print (totalRqN,xMedian,xUp,xDown)
 msgTwo = input("Press ENTER to continue: ")
 quit()
-#Let's get Qoil values
-Qoil = float(input(""))
