904089fc668bf7f10303526f6c558e3bb324c176
Anchrone <anchrone@gmail.com>
2018-11-17 18:57:15 +0400

    Update ngraph.py

--- ngraph.py
+++ ngraph.py
@@ -1,4 +1,4 @@
-#N graph
+#Ngraph v.1.5
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,9 +21,31 @@ for a in range(productionStartYear,productionEndYear+1):
         years.append(year_of_Qn)
         xQn = float(input("What is discharge? "))
         discharges.append(xQn)
-for x in range (years[1], productionTimeDif):
-                dischargesC.append(xQn)
+        
+        
+rQn = [abs(y-x) for x,y in zip(discharges, discharges[1:])]
+
+totalRqN = ((sum(rQn))/productionTimeDif)
+xMedian = ((sum(discharges))/productionTimeDif)
+xUp = (xMedian + ((3 * totalRqN)/1.128))
+xDown = (abs(xMedian - ((3 * totalRqN)/1.128)))
+dischargeUpCoef = [xUp] * len(years)
+dischargeDownCoef = [xDown] * len(years)
+dischargeMedianCoef =  [xMedian] * len(years)
+plt.plot(years, discharges, label ="Q", color = "blue")
+plt.plot(years, dischargeUpCoef, label = "x UP", color = "green")
+plt.plot(years, dischargeDownCoef, label = "x DOWN", color = "red")
+plt.plot(years, dischargeMedianCoef, label = "x MEDIAN", color ="yellow")
+plt.title("Q of oil graph")
+plt.legend()
+plt.show()
+
+#for x in range (years[1], productionTimeDif):
+ #               dischargesC.append(xQn)
  #       
+
+
+
 ##ReachB =  discharges[0:productionTimeDif]
 #reachMax = list(np.array(ReachG)-np.array(ReachB))
 
@@ -31,13 +53,15 @@ for x in range (years[1], productionTimeDif):
 #dischargesC = (discharges)
 #del dischargesC[0]
 
-reachNew = [dischargesC - discharges for (dischargesC,discharges) in zip(discharges,dischargesC)]
 
 #need to find xmedian, R, xUp and xDown     
 #xmedian = (sum(discharges)/productionTimeDif)
 #reachSum = (sum(reachMax)/(productionTimeDif - 1))
 #print (xmedian)
-print (reachNew)
+msgOne = input("Press ENTER to look at your values: ")
+
+print (totalRqN,xMedian,xUp,xDown)
+msgTwo = input("Press ENTER to continue: ")
+quit()
 #Let's get Qoil values
-print 
 Qoil = float(input(""))
